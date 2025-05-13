f16eb4db4196b892a9b9723f9435836031a4165d
prothegee <prothegee@proton.me>
2025-05-13 14:29:50 +0700

    adding completed lib for couchbase cxx client

--- src/becc.cc
+++ src/becc.cc
@@ -1,2 +1,13 @@
+#include <couchbase/cluster.hxx>
+#include <couchbase/codec/tao_json_serializer.hxx>
+#include <couchbase/logger.hxx>
+
+#include <spdlog/fmt/bundled/core.h>
+
+#include <couchbase/fmt/error.hxx>
+
+#include <iostream>
+#include <system_error>
+
 namespace becc {
 } // namespace becc
